86a26ee2dc3bafd391ac639c59fefc0f3e397b6c
zzy17667036 <17667036@qq.com>
2021-04-05 12:23:27 +0800

    blog中的models.py（与内容直接相关）

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Category(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
-    STATUS_ITEMS =(
+    STATUS_ITEMS = (
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除'),
     )
@@ -31,7 +31,37 @@ class Tag(models.Model):
     )
 
     name = models.CharField(max_length=10, verbose_name='名称')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL,
+                                         choices=STATUS_ITEMS,
+                                         verbose_name='状态')
+    owner = models.ForeignKey(User, verbose_name='作者', on_delete='CASCADE')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    class Meta:
+        verbose_name = verbose_name_plural = '标签'
 
 
+class Psot(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_DRAFT = 2
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+        (STATUS_DRAFT, '草稿'),
+    )
 
+    title = models.CharField(max_length=255, verbose_name='标题')
+    desc = models.CharField(max_length=1024, blank=True, verbose_name='摘要')
+    content = models.TextField(verbose_name='正文', help_text='正文必须为MarkDown格式')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL,
+                                         choices=STATUS_ITEMS,
+                                         verbose_name='状态')
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete='CASCADE')
+    tag = models.ManyToManyField(Tag, verbose_name='标签')
+    owner = models.ForeignKey(User, verbose_name='作者', on_delete='CASCADE')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
+    class Meta:
+        verbose_name = verbose_name_plural = '文章'
+        ordering = ['-id']   # 根据id进行降序排列
